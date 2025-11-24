416b359879b6bdf2b33852b4f02684c8d2beb396
Khanh Le <tommapcoder@gmail.com>
2025-11-24 23:32:03 +0700

    init eazystore-be

---
+++ eazystore-be/src/main/java/org/tommap/eazystorebe/EazystoreBeApplication.java
@@ -0,0 +1,13 @@
+package org.tommap.eazystorebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazystoreBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EazystoreBeApplication.class, args);
+    }
+
+}
---
+++ eazystore-be/src/test/java/org/tommap/eazystorebe/EazystoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tommap.eazystorebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazystoreBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
