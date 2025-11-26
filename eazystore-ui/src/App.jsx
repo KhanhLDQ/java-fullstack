17120d852d927866229acc090aa6b5b0f8480891
Khanh Le <tommapcoder@gmail.com>
2025-11-26 22:38:29 +0700

    react fragment

--- eazystore-ui/src/App.jsx
+++ eazystore-ui/src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/footer/Footer";
+import React from "react";
 import ClassHeader from "./components/ClassHeader";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <Footer />
-    </div>
+    </>
   );
 }
 
