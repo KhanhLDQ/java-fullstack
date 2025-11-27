8c7e7b0fe0a306f2f393e291050e2faaa4534f73
Khanh Le <tommapcoder@gmail.com>
2025-11-27 08:45:59 +0700

    build page heading|title components

--- eazystore-ui/src/App.jsx
+++ eazystore-ui/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/footer/Footer";
 import React from "react";
+import Home from "./components/Home";
 import ClassHeader from "./components/ClassHeader";
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   );
