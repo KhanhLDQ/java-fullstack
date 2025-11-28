035a53e4615d47bbe84a838b67fd10c3c939bd0f
Khanh Le <tommapcoder@gmail.com>
2025-11-28 18:17:04 +0700

    demo utility classes in tailwind

--- basic-tailwind/src/App.jsx
+++ basic-tailwind/src/App.jsx
@@ -4,6 +4,85 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <p className="text-lg underline font-bold mt-2 text-blue-400">
+        Tailwind makes styling easy!!
+      </p>
+
+      <div className="bg-gray-200 p-2 m-2">Background color demo</div>
+
+      <div className="bg-green-300 border border-green-600 p-2 rounded-lg m-2">
+        Border demo
+      </div>
+
+      <div className="grid grid-cols-3 gap-2 mt-2">
+        <div className="bg-blue-200 p-2 text-red-500">1</div>
+        <div className="bg-blue-300 p-2">2</div>
+        <div className="bg-blue-400 p-2 text-white">3</div>
+      </div>
+
+      <div className="flex justify-center items-center gap-2 h-20 bg-gray-400 mt-2 flex-row">
+        <p className="text-xl font-bold">Text I</p>
+        <p className="text-xl font-bold">Text II</p>
+        <p className="text-xl font-bold">Text III</p>
+      </div>
+
+      <table className="table-auto w-full border-collapse border mt-2 border-gray-300 shadow-lg rounded-lg">
+        <thead>
+          <tr className="bg-gray-200 text-gray-700 uppercase text-sm leading-normal">
+            <th className="border border-gray-300 px-3 py-1.5 text-center">
+              Song
+            </th>
+            <th className="border border-gray-300 px-3 py-1.5 text-center">
+              Artist
+            </th>
+            <th className="border border-gray-300 px-3 py-1.5 text-center">
+              Year
+            </th>
+          </tr>
+        </thead>
+        <tbody className="text-gray-600 text-sm font-light">
+          <tr className="border-b border-gray-200 hover:bg-gray-100 transition">
+            <td className="px-3 py-1.5 border border-gray-300">
+              The Sliding Mr. Bones (Next Stop, Pottersville)
+            </td>
+            <td className="px-3 py-1.5 border border-gray-300">
+              Malcolm Lockyer
+            </td>
+            <td className="px-3 py-1.5 border border-gray-300">1961</td>
+          </tr>
+          <tr className="border-b border-gray-200 hover:bg-gray-100 transition">
+            <td className="px-3 py-1.5 border border-gray-300">Witchy Woman</td>
+            <td className="px-3 py-1.5 border border-gray-300">The Eagles</td>
+            <td className="px-3 py-1.5 border border-gray-300">1972</td>
+          </tr>
+          <tr className="border-b border-gray-200 hover:bg-gray-100 transition">
+            <td className="px-3 py-1.5 border border-gray-300">Shining Star</td>
+            <td className="px-3 py-1.5 border border-gray-300">
+              Earth, Wind, and Fire
+            </td>
+            <td className="px-3 py-1.5 border border-gray-300">1975</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <button className="px-3 py-1 m-2 bg-blue-200 text-white rounded-lg hover:bg-red-300 transition duration-300">
+        Click Me!!
+      </button>
+
+      <button className="px-3 py-1 m-2 bg-red-200 text-white rounded-lg hover:bg-gray-300 transition duration-500 ease-in-out transform hover:scale-110">
+        Touch Me!!
+      </button>
+
+      <div className="rounded-lg shadow-lg bg-indigo-100 p-3 max-w-sm">
+        <h2 className="text-2xl font-bold mb-2">Tailwind Card</h2>
+        <p className="text-gray-700">
+          This is a simple card built with Tailwind CSS
+        </p>
+        <button className="px-3 py-1 m-2 bg-blue-400 text-white rounded-lg hover:bg-red-300 transition duration-500 ease-in-out transform hover:scale-110">
+          Read More!
+        </button>
+      </div>
     </>
   );
 }
