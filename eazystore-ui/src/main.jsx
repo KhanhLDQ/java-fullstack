86a38704ddc92f75fbc731688dfd57c86eeff6b1
Khanh Le <tommapcoder@gmail.com>
2025-11-30 17:11:23 +0700

    intro strict mode

--- eazystore-ui/src/main.jsx
+++ eazystore-ui/src/main.jsx
@@ -5,6 +5,11 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    {/* StrictMode helps catch bugs in development by:
+        - Running components twice to detect side effects
+        - Warning about deprecated APIs
+        - Checking for unsafe lifecycles
+        Note: Only affects development, not production builds */}
     <App />
   </StrictMode>
 );
