b09a228efb21624c5bdb5b10f4e587835deb26b3
Khanh Le <tommapcoder@gmail.com>
2025-12-01 15:51:18 +0700

    route definitions via outlet

--- eazystore-ui/src/App.jsx
+++ eazystore-ui/src/App.jsx
@@ -2,13 +2,17 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import React from "react";
-import Home from "./components/Home";
+import { Outlet } from "react-router-dom";
 
+//React will not load the entire pages from the server
+//Instead, it only renders the corresponding components that represent each page
+// -> that's why it can be called SPA
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      {/* dynaic child route components */}
+      <Outlet />
       <Footer />
     </>
   );
--- eazystore-ui/src/main.jsx
+++ eazystore-ui/src/main.jsx
@@ -2,6 +2,48 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import Login from "./components/Login.jsx";
+import Cart from "./components/Cart.jsx";
+import Home from "./components/Home.jsx";
+import ErrorPage from "./components/ErrorPage.jsx";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      //tell to react router lib -> when someone's trying to access the base path - load the Home component as well
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -10,6 +52,7 @@ createRoot(document.getElementById("root")).render(
         - Warning about deprecated APIs
         - Checking for unsafe lifecycles
         Note: Only affects development, not production builds */}
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={appRouter} />
   </StrictMode>
 );
